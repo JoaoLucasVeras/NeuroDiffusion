faa3deab943304d787567ec1cf178e34f4ca332a
JoaoLucasVeras-ImpacterAI <Joao@impacteragi.com>
2026-04-24 14:51:27 -0700

    Prepare for HPC migration: Update configs, add SLURM script, and fix dependencies

--- code/config.py
+++ code/config.py
@@ -15,9 +15,9 @@ class Config_MBM_EEG(Config_MAE_fMRI):
         self.lr = 2.5e-4
         self.min_lr = 0.
         self.weight_decay = 0.05
-        self.num_epoch = 500
-        self.warmup_epochs = 40
-        self.batch_size = 100
+        self.num_epoch = 10
+        self.warmup_epochs = 2
+        self.batch_size = 8
         self.clip_grad = 0.8
         
         # Model Parameters
@@ -31,8 +31,8 @@ class Config_MBM_EEG(Config_MAE_fMRI):
         self.mlp_ratio = 1.0
 
         # Project setting
-        self.root_path = '../dreamdiffusion/'
-        self.output_path = '../dreamdiffusion/exps/'
+        self.root_path = '../'
+        self.output_path = '../exps/'
         self.seed = 2022
         self.roi = 'VC'
         self.aug_times = 1
@@ -54,9 +54,9 @@ class Config_EEG_finetune(Config_MBM_finetune):
     def __init__(self):
         
         # Project setting
-        self.root_path = '../dreamdiffusion/'
+        self.root_path = '../'
         # self.root_path = '.'
-        self.output_path = '../dreamdiffusion/exps/'
+        self.output_path = '../exps/'
 
         self.eeg_signals_path = os.path.join(self.root_path, 'datasets/eeg_5_95_std.pth')
         self.splits_path = os.path.join(self.root_path, 'datasets/block_splits_by_image_all.pth')
@@ -89,8 +89,8 @@ class Config_Generative_Model:
     def __init__(self):
         # project parameters
         self.seed = 2022
-        self.root_path = '../dreamdiffusion/'
-        self.output_path = '../dreamdiffusion/exps/'
+        self.root_path = '../'
+        self.output_path = '../exps/'
 
         self.eeg_signals_path = os.path.join(self.root_path, 'datasets/eeg_5_95_std.pth')
         self.splits_path = os.path.join(self.root_path, 'datasets/block_splits_by_image_single.pth')
@@ -139,8 +139,8 @@ class Config_Cls_Model:
     def __init__(self):
         # project parameters
         self.seed = 2022
-        self.root_path = '../dreamdiffusion/'
-        self.output_path = '../dreamdiffusion/exps/'
+        self.root_path = '../'
+        self.output_path = '../exps/'
 
         # self.eeg_signals_path = os.path.join(self.root_path, 'datasets/eeg_5_95_std.pth')
         self.eeg_signals_path = os.path.join(self.root_path, 'datasets/eeg_14_70_std.pth')
