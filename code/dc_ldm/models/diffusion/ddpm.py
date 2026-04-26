6b398cdbbe1784693a8c7e7556d584bab431222c
JoaoLucasVeras-ImpacterAI <Joao@impacteragi.com>
2026-04-25 19:04:48 -0700

    Fix PyTorch register_buffer KeyError: initialization collision resolved

--- code/dc_ldm/models/diffusion/ddpm.py
+++ code/dc_ldm/models/diffusion/ddpm.py
@@ -113,11 +113,11 @@ class DDPM(pl.LightningModule):
         self.loss_type = loss_type
 
         self.learn_logvar = learn_logvar
-        self.logvar = torch.full(fill_value=logvar_init, size=(self.num_timesteps,))
+        logvar = torch.full(fill_value=logvar_init, size=(self.num_timesteps,))
         if self.learn_logvar:
-            self.logvar = nn.Parameter(self.logvar, requires_grad=True)
+            self.logvar = nn.Parameter(logvar, requires_grad=True)
         else:
-            self.register_buffer('logvar', self.logvar)
+            self.register_buffer('logvar', logvar)
 
         self.validation_count = 0
         self.ddim_steps = ddim_steps
