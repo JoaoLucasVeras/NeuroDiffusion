e70dd45533e1ad9f7b376c67bf31b1248fa5a0d8
JoaoLucasVeras-ImpacterAI <Joao@impacteragi.com>
2026-07-01 10:17:17 -0700

    Update check dataset to print image names

--- check_dataset.py
+++ check_dataset.py
@@ -1,10 +1,4 @@
 import torch
 eeg = torch.load('datasets/eeg_5_95_std.pth')
-print("Sample 0 keys:", eeg['dataset'][0].keys())
-print("Sample 0 image type:", type(eeg['dataset'][0]['image']))
-print("Sample 0 image value:", eeg['dataset'][0]['image'])
-if isinstance(eeg.get('images', None), list):
-    print("Images list length:", len(eeg['images']))
-    print("Images list first 5:", eeg['images'][:5])
-elif eeg.get('images', None) is not None:
-    print("Images object type:", type(eeg['images']))
+print("Total images in self.images:", len(eeg['images']))
+print("First 10 images:", eeg['images'][:10])
