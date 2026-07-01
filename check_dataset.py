0f67461f70b460d2061fda7de896d9379626a59e
JoaoLucasVeras-ImpacterAI <Joao@impacteragi.com>
2026-07-01 10:43:20 -0700

    Update check dataset to print keys

--- check_dataset.py
+++ check_dataset.py
@@ -1,4 +1,3 @@
 import torch
 eeg = torch.load('datasets/eeg_5_95_std.pth')
-print("Total images in self.images:", len(eeg['images']))
-print("First 10 images:", eeg['images'][:10])
+print("Keys in eeg:", eeg.keys())
