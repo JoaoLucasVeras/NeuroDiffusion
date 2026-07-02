b145cba9d51970093cc31e9e03172b96d533496e
JoaoLucasVeras-ImpacterAI <Joao@impacteragi.com>
2026-07-01 18:25:33 -0700

    Request 2 GPUs to bypass broken GPU 0

--- hpc_submit_stage2.sh
+++ hpc_submit_stage2.sh
@@ -4,12 +4,13 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --time=24:00:00
 
 
 export TRANSFORMERS_OFFLINE=1
 export HF_DATASETS_OFFLINE=1
 export WANDB_MODE=offline
+export CUDA_VISIBLE_DEVICES=1
 
 ~/.conda/envs/neurodiffusion/bin/python code/eeg_ldm.py
